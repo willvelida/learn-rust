1ce912c72d5266e6243959ae28c7ab861bd223bb
Will Velida <willvelida@hotmail.co.uk>
2026-07-11 13:57:22 +1000

    Adding lesson 13: wordtally project to repository
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

---
+++ wordtally/src/main.rs
@@ -0,0 +1,14 @@
+use std::process;
+use wordtally::Config;
+
+fn main() {
+    let config = Config::build(std::env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = wordtally::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
